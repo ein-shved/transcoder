f7782f423342121b5944f1bd86ce6158d36cf594
Yury Shvedov <mestofel13@gmail.com>
2025-09-28 12:13:30 +0300

    chore: use log and env_logger for logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
+use log::info;
 use tokio;
-use transcoder::watcher::{Watcher, WatchPair};
+use transcoder::watcher::{WatchPair, Watcher};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -10,11 +11,12 @@ struct Args {
 
 #[tokio::main]
 async fn main() {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let args = Args::parse();
 
     let mut watcher = Watcher::new();
     for pair in std::iter::once(args.pair).chain(args.pairs.into_iter()) {
-        println!("Watching {:?} -> {:?}", pair.src, pair.dst);
+        info!("Watching {:?} -> {:?}", pair.src, pair.dst);
         watcher.add(pair).unwrap();
     }
     watcher.watch().await;
