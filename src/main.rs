6fdc5dfb5bb3c6f45f3ff38185948f1758a65dbf
Yury Shvedov <mestofel13@gmail.com>
2025-11-13 22:57:31 +0300

    simple: clearing code to simplify transcoding
    This action is perform to throw out all code and write simple
    decoder-encoder to debug the correctness of target transcoder.

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,18 @@
 use clap::Parser;
 use log::{debug, info};
-use std::fs::File;
-use std::io::Read;
 use std::path::PathBuf;
-use std::process::Stdio;
-use tokio;
-use transcoder::transcoder::TranscoderConfig;
-use transcoder::watcher::{WatchPair, Watcher};
+use transcoder;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[arg(short, long, required = true)]
-    config: PathBuf,
-    pair: WatchPair,
-    pairs: Vec<WatchPair>,
+    input: PathBuf,
+    output: PathBuf
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let args = Args::parse();
 
-    let config_type = args
-        .config
-        .extension()
-        .expect("Unknown config file type")
-        .to_str()
-        .expect("Unknown config file type")
-        .to_lowercase();
-    let mut reader = File::open(&args.config).unwrap();
+    todo!()
 
-    let config: TranscoderConfig = if config_type == "toml" {
-        let mut s = String::new();
-        reader.read_to_string(&mut s).unwrap();
-        toml::from_str(&s).unwrap()
-    } else if config_type == "json" {
-        serde_json::from_reader(reader).unwrap()
-    } else if config_type == "yaml" {
-        serde_yaml::from_reader(reader).unwrap()
-    } else if config_type == "nix" {
-        let stdout = std::process::Command::new("nix-instantiate")
-            .args(["--eval", "--json", "--strict"])
-            .arg(&args.config)
-            .stderr(Stdio::inherit())
-            .output()
-            .expect("Unable to process nix config").stdout;
-        let json = String::from_utf8(stdout).unwrap();
-        serde_json::from_str(&json).unwrap()
-    } else {
-        panic!("Unsupported config type ${config_type}")
-    };
-    debug!("Configuration: {config:#?}");
-    TranscoderConfig::set(config);
-
-    ffmpeg_next::init().unwrap();
-    ffmpeg_next::log::set_level(ffmpeg_next::log::Level::Debug);
-    let mut watcher = Watcher::new();
-    for pair in std::iter::once(args.pair).chain(args.pairs.into_iter()) {
-        info!("Watching {:?} -> {:?}", pair.src, pair.dst);
-        watcher.add(pair).unwrap();
-    }
-    watcher.watch().await;
 }
